8f2bf9f35a9c42ec7acc4db95c2fb7bc425035d0
Lav Kushwaha <lavkumar1224@gmail.com>
2023-07-24 16:19:15 +0530

    Added Order and Stripe Payment Method

--- client/src/Components/Cart/Cart.jsx
+++ client/src/Components/Cart/Cart.jsx
@@ -1,43 +1,70 @@
-import React from 'react';
-import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import React from "react";
 import "./Cart.scss";
-import {useDispatch, useSelector} from "react-redux"
-import { removeItem } from '../../redux/cartReducer';
-import {resetCart} from '../../redux/cartReducer';
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import { useSelector } from "react-redux";
+import { removeItem, resetCart } from "../../redux/cartReducer";
+import { useDispatch } from "react-redux";
+import { makeRequest } from "../../makeRequest";
+import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
-  const products = useSelector(state=>state.cart.products)
-  const dispatch=useDispatch();
+  const products = useSelector((state) => state.cart.products);
+  const dispatch = useDispatch();
 
-  const totalPrice = ()=>{
+  const totalPrice = () => {
     let total = 0;
-    products.forEach((item)=>(total+=item.quantity*item.price))
+    products.forEach((item) => {
+      total += item.quantity * item.price;
+    });
     return total.toFixed(2);
-  }
+  };
+
+  const stripePromise = loadStripe(
+    "pk_test_51NX5FGSFSiY8hTPLobTKEGVaH7LL4B44GxRCQnsJqerGmmvUPl6kEpVuL7PrUvfO7AA9X0t82BlqQmUhUBOwQwFN00Sq72G1UC"
+  );
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res = await makeRequest.post("/orders", {
+        products,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <div className='cart'>
-        <h1>Products in your cart</h1>
-        {products?.map(item=>(
-            <div className="item" key={item.id}>
-               <img src={process.env.REACT_APP_UPLOAD_URL+item.img} alt="img"/> 
-               <div className="details">
-                <h1>{item.title}</h1>
-                <p>{item.desc.substring(0,40)}</p>
-                <div className="price">
-                    {item.quantity} × ₹{item.price}
-                </div>
-               </div>
-               <DeleteOutlinedIcon className="delete" onClick={()=>dispatch(removeItem(item.id))}/>
+    <div className="cart">
+      <h1>Products in your cart</h1>
+      {products?.map((item) => (
+        <div className="item" key={item.id}>
+          <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="img" />
+          <div className="details">
+            <h1>{item.title}</h1>
+            <p>{item.desc?.substring(0, 100)}</p>
+            <div className="price">
+              {item.quantity} x ₹ {item.price}
             </div>
-        ))}
-        <div className="total">
-            <span>SUBTOTAL</span>
-            <span>₹{totalPrice()}</span>
+          </div>
+          <DeleteOutlinedIcon
+            className="delete"
+            onClick={() => dispatch(removeItem(item.id))}
+          />
         </div>
-        <button>PROCEED TO CHECKOUT</button>
-        <span className="reset" onClick={()=>dispatch(resetCart())}>Reset Cart</span>
+      ))}
+      <div className="total">
+        <span>TOTAL PRICE</span>
+        <span>₹ {totalPrice()}</span>
+      </div>
+      <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
+      <span className="reset" onClick={() => dispatch(resetCart())}>
+        Reset Cart
+      </span>
     </div>
-  )
-}
+  );
+};
 
 export default Cart;
